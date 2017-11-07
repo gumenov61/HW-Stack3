c49dcc931a69ffd114dac2e56dc69756a2167fe2
gumenov61 <32020798+gumenov61@users.noreply.github.com>
2017-11-08 02:17:33 +0300

    Add files via upload

---
+++ sources/main.cpp
@@ -0,0 +1,17 @@
+//FUNC MAIN
+#include "stack.hpp"
+
+//ДЕКЛАРАЦИЯ ФУНКЦИЙ ДЛЯ ПРИМЕНЕНИЯ МЕТОДОВ
+void foo_init();
+void foo_push();
+void foo_pop();
+void foo_empty();
+
+int main() {
+	foo_init();
+	foo_push();
+	foo_pop();
+	foo_empty();
+	
+	return 0;
+}
